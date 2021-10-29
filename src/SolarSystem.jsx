e8e3b1355cb2d39fa88d5aaa09782eef132ef3ec
cdartora <cdartora>
2021-10-29 18:37:08 -0300

    refactor[SolarSystem]: include Title component in the structure

--- src/SolarSystem.jsx
+++ src/SolarSystem.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import data from './data/planets';
+import Title from './Title'
 
 class SolarSystem extends Component {
   render() { 
 
     return (
       <div data-testid="solar-system">
+        
+        <Title headline='Planetas' />
 
-        {data.map(planet => {
-
+        {data.map(planet => { // map that renders every planet dinamically
           return (
             <>
-              <h3>{planet.name}</h3>
+              <h4>{planet.name}</h4>
               <img src={planet.image} alt={planet.name} />
             </>
           )
-
         })}
 
       </div>
